54355b441c2442b273d49dd8d32f805852839142
Abhay Sharma <abhays948@gmail.com>
2020-04-17 01:45:23 +0530

    fourth

--- tyreupsizer.js
+++ tyreupsizer.js
@@ -12,12 +12,14 @@ var r1=((bs/100)*bw)+br*12.7;
 	var dif=((d2-d1)*100/d1).toFixed(2);
 	var ans=dif;
 	var speed=(d2*100/d1).toFixed(2);
+	var radi=(r2-r1).toFixed(2);
 	// alert(dif*100)
     (dif<=3&&dif>=-3)?
 	(document.getElementById("difference1").innerHTML="the total diameter variation is  "+dif+"% ,you can safely upgrade without largely affecting the drive dynamics")
 :
 	(document.getElementById("difference1").innerHTML="the total diameter variation is  "+dif+"% ,you can not safely upgrade")
 document.getElementById("difference2").innerHTML="If you are travelling at a speed of 100kmph then your actual speed would be "+speed+"kmph"
+document.getElementById("difference3").innerHTML="the ground clearance of your car will increase by "+radi+"mm"
 }
 function scrollWin() {
   window.scrollTo(0, 5000);
